92aacfadb9590584fd7638db3dc6881749dc7205
Lucoal <94873749+Lucoal@users.noreply.github.com>
2022-07-22 07:43:13 +0200

    Add files via upload

---
+++ File Mover/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace File_Mover
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.fromPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toPath = new System.Windows.Forms.TextBox();
+            this.btnOpen2 = new System.Windows.Forms.Button();
+            this.btnSwitch = new System.Windows.Forms.Button();
+            this.btnMove = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "From:";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(450, 15);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // fromPath
+            // 
+            this.fromPath.Location = new System.Drawing.Point(60, 15);
+            this.fromPath.Name = "fromPath";
+            this.fromPath.ReadOnly = true;
+            this.fromPath.Size = new System.Drawing.Size(384, 23);
+            this.fromPath.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "To:";
+            // 
+            // toPath
+            // 
+            this.toPath.Location = new System.Drawing.Point(60, 62);
+            this.toPath.Name = "toPath";
+            this.toPath.ReadOnly = true;
+            this.toPath.Size = new System.Drawing.Size(384, 23);
+            this.toPath.TabIndex = 4;
+            // 
+            // btnOpen2
+            // 
+            this.btnOpen2.Location = new System.Drawing.Point(450, 62);
+            this.btnOpen2.Name = "btnOpen2";
+            this.btnOpen2.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen2.TabIndex = 5;
+            this.btnOpen2.Text = "Open";
+            this.btnOpen2.UseVisualStyleBackColor = true;
+            this.btnOpen2.Click += new System.EventHandler(this.btnOpen2_Click);
+            // 
+            // btnSwitch
+            // 
+            this.btnSwitch.Location = new System.Drawing.Point(187, 98);
+            this.btnSwitch.Name = "btnSwitch";
+            this.btnSwitch.Size = new System.Drawing.Size(88, 23);
+            this.btnSwitch.TabIndex = 6;
+            this.btnSwitch.Text = "Switch Path";
+            this.btnSwitch.UseVisualStyleBackColor = true;
+            this.btnSwitch.Click += new System.EventHandler(this.btnSwitch_Click);
+            // 
+            // btnMove
+            // 
+            this.btnMove.Location = new System.Drawing.Point(450, 100);
+            this.btnMove.Name = "btnMove";
+            this.btnMove.Size = new System.Drawing.Size(75, 23);
+            this.btnMove.TabIndex = 7;
+            this.btnMove.Text = "Move";
+            this.btnMove.UseVisualStyleBackColor = true;
+            this.btnMove.Click += new System.EventHandler(this.btnMove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 133);
+            this.Controls.Add(this.btnMove);
+            this.Controls.Add(this.btnSwitch);
+            this.Controls.Add(this.btnOpen2);
+            this.Controls.Add(this.toPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.fromPath);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "File Mover";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button btnOpen;
+        private TextBox fromPath;
+        private Label label2;
+        private TextBox toPath;
+        private Button btnOpen2;
+        private Button btnSwitch;
+        private Button btnMove;
+    }
+}
---
+++ File Mover/Form1.cs
@@ -0,0 +1,66 @@
+using System.Diagnostics;
+
+namespace File_Mover
+{
+    public partial class Form1 : Form
+    {
+
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+        }
+
+        public string fileName = "";
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                fromPath.Text = ofd.FileName;
+                fileName = Path.GetFileName(fromPath.Text);
+    }
+
+
+        }
+
+        private void btnOpen2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if(fbd.ShowDialog() == DialogResult.OK)
+            {
+                toPath.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void btnSwitch_Click(object sender, EventArgs e)
+        {
+            string temp = $"{toPath.Text}\\{fileName}";
+            toPath.Text = Path.GetDirectoryName(fromPath.Text);
+            fromPath.Text = temp;
+            
+        }
+
+        private void btnMove_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                File.Move(fromPath.Text, toPath.Text + $"\\{fileName}");
+                MessageBox.Show($"{fileName} has been moved!");
+                
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error");
+            }
+            
+        }
+    }
+}
