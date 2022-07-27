7734eed7231cf88cda065a2e899428facdee3321
Luca Ardò <l.ardo@hotmail.it>
2022-07-27 20:59:40 +0200

    new ui and small patch

--- File Mover/Form1.Designer.cs
+++ File Mover/Form1.Designer.cs
@@ -37,22 +37,32 @@
             this.btnOpen2 = new System.Windows.Forms.Button();
             this.btnSwitch = new System.Windows.Forms.Button();
             this.btnMove = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.minimizeButton = new System.Windows.Forms.Button();
+            this.TitleMenuPanel = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TitleMenuPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 18);
+            this.label1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.label1.Location = new System.Drawing.Point(28, 57);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.Size = new System.Drawing.Size(46, 14);
             this.label1.TabIndex = 0;
             this.label1.Text = "From:";
             // 
             // btnOpen
             // 
-            this.btnOpen.Location = new System.Drawing.Point(450, 15);
+            this.btnOpen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOpen.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnOpen.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnOpen.Location = new System.Drawing.Point(465, 54);
             this.btnOpen.Name = "btnOpen";
-            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.Size = new System.Drawing.Size(75, 24);
             this.btnOpen.TabIndex = 1;
             this.btnOpen.Text = "Open";
             this.btnOpen.UseVisualStyleBackColor = true;
@@ -60,8 +70,9 @@
             // 
             // fromPath
             // 
-            this.fromPath.Location = new System.Drawing.Point(60, 15);
+            this.fromPath.Location = new System.Drawing.Point(75, 54);
             this.fromPath.Name = "fromPath";
+            this.fromPath.PlaceholderText = "Select a file to move...";
             this.fromPath.ReadOnly = true;
             this.fromPath.Size = new System.Drawing.Size(384, 23);
             this.fromPath.TabIndex = 2;
@@ -69,25 +80,31 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 65);
+            this.label2.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.label2.Location = new System.Drawing.Point(31, 104);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(24, 15);
+            this.label2.Size = new System.Drawing.Size(28, 14);
             this.label2.TabIndex = 3;
             this.label2.Text = "To:";
             // 
             // toPath
             // 
-            this.toPath.Location = new System.Drawing.Point(60, 62);
+            this.toPath.Location = new System.Drawing.Point(75, 101);
             this.toPath.Name = "toPath";
+            this.toPath.PlaceholderText = "Select destination folder...";
             this.toPath.ReadOnly = true;
             this.toPath.Size = new System.Drawing.Size(384, 23);
             this.toPath.TabIndex = 4;
             // 
             // btnOpen2
             // 
-            this.btnOpen2.Location = new System.Drawing.Point(450, 62);
+            this.btnOpen2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOpen2.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnOpen2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnOpen2.Location = new System.Drawing.Point(465, 101);
             this.btnOpen2.Name = "btnOpen2";
-            this.btnOpen2.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen2.Size = new System.Drawing.Size(75, 24);
             this.btnOpen2.TabIndex = 5;
             this.btnOpen2.Text = "Open";
             this.btnOpen2.UseVisualStyleBackColor = true;
@@ -95,9 +112,12 @@
             // 
             // btnSwitch
             // 
-            this.btnSwitch.Location = new System.Drawing.Point(187, 98);
+            this.btnSwitch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSwitch.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSwitch.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnSwitch.Location = new System.Drawing.Point(224, 143);
             this.btnSwitch.Name = "btnSwitch";
-            this.btnSwitch.Size = new System.Drawing.Size(88, 23);
+            this.btnSwitch.Size = new System.Drawing.Size(88, 24);
             this.btnSwitch.TabIndex = 6;
             this.btnSwitch.Text = "Switch Path";
             this.btnSwitch.UseVisualStyleBackColor = true;
@@ -105,19 +125,81 @@
             // 
             // btnMove
             // 
-            this.btnMove.Location = new System.Drawing.Point(450, 100);
+            this.btnMove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMove.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnMove.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnMove.Location = new System.Drawing.Point(465, 143);
             this.btnMove.Name = "btnMove";
-            this.btnMove.Size = new System.Drawing.Size(75, 23);
+            this.btnMove.Size = new System.Drawing.Size(75, 24);
             this.btnMove.TabIndex = 7;
             this.btnMove.Text = "Move";
             this.btnMove.UseVisualStyleBackColor = true;
             this.btnMove.Click += new System.EventHandler(this.btnMove_Click);
             // 
+            // exitButton
+            // 
+            this.exitButton.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.exitButton.FlatAppearance.BorderSize = 0;
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitButton.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.exitButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.exitButton.Location = new System.Drawing.Point(530, 2);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(0);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(24, 24);
+            this.exitButton.TabIndex = 8;
+            this.exitButton.Text = "X";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // minimizeButton
+            // 
+            this.minimizeButton.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.minimizeButton.FlatAppearance.BorderSize = 0;
+            this.minimizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minimizeButton.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.minimizeButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.minimizeButton.Location = new System.Drawing.Point(503, 2);
+            this.minimizeButton.Name = "minimizeButton";
+            this.minimizeButton.Size = new System.Drawing.Size(24, 24);
+            this.minimizeButton.TabIndex = 9;
+            this.minimizeButton.Text = "_";
+            this.minimizeButton.UseVisualStyleBackColor = true;
+            this.minimizeButton.Click += new System.EventHandler(this.minimizeButton_Click);
+            // 
+            // TitleMenuPanel
+            // 
+            this.TitleMenuPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.TitleMenuPanel.Controls.Add(this.label3);
+            this.TitleMenuPanel.Controls.Add(this.minimizeButton);
+            this.TitleMenuPanel.Controls.Add(this.exitButton);
+            this.TitleMenuPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TitleMenuPanel.Location = new System.Drawing.Point(0, 0);
+            this.TitleMenuPanel.Name = "TitleMenuPanel";
+            this.TitleMenuPanel.Size = new System.Drawing.Size(574, 29);
+            this.TitleMenuPanel.TabIndex = 10;
+            this.TitleMenuPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mouseDown_Event);
+            this.TitleMenuPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mouseMove_Event);
+            this.TitleMenuPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.mouseUp_Event);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.label3.Location = new System.Drawing.Point(28, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 23);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "File Mover";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(541, 133);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(574, 184);
+            this.Controls.Add(this.TitleMenuPanel);
             this.Controls.Add(this.btnMove);
             this.Controls.Add(this.btnSwitch);
             this.Controls.Add(this.btnOpen2);
@@ -126,10 +208,16 @@
             this.Controls.Add(this.fromPath);
             this.Controls.Add(this.btnOpen);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "File Mover";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mouseDown_Event);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mouseMove_Event);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.mouseUp_Event);
+            this.TitleMenuPanel.ResumeLayout(false);
+            this.TitleMenuPanel.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -145,5 +233,9 @@
         private Button btnOpen2;
         private Button btnSwitch;
         private Button btnMove;
+        private Button exitButton;
+        private Button minimizeButton;
+        private Panel TitleMenuPanel;
+        private Label label3;
     }
 }
--- File Mover/Form1.cs
+++ File Mover/Form1.cs
@@ -1,12 +1,26 @@
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace File_Mover
 {
     public partial class Form1 : Form
     {
+        //round window
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+
+        private static extern IntPtr CreateRoundRectRgn
+            (
+            int nLeftRect,
+            int nTopRect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+            );
         public Form1()
         {
             InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
         }
 
         public string fileName = "";
@@ -35,22 +49,68 @@ namespace File_Mover
 
         private void btnSwitch_Click(object sender, EventArgs e)
         {
-            string temp = $"{toPath.Text}\\{fileName}";
-            toPath.Text = Path.GetDirectoryName(fromPath.Text);
-            fromPath.Text = temp; 
+            if(toPath.Text != "")
+            {
+                string temp = $"{toPath.Text}\\{fileName}";
+                toPath.Text = Path.GetDirectoryName(fromPath.Text);
+                fromPath.Text = temp;
+            }
+            else
+            {
+                MessageBox.Show("Destination path is null", "Alert");
+            }
+            
         }
+
 
         private void btnMove_Click(object sender, EventArgs e)
         {
             try
             {
                 File.Move(fromPath.Text, toPath.Text + $"\\{fileName}");
-                MessageBox.Show($"{fileName} has been moved!"); 
+                MessageBox.Show($"{fileName} has been moved!");
             }
             catch (Exception)
             {
-                MessageBox.Show("Error");
+                MessageBox.Show("File can't be moved", "Error");
             }
         }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void minimizeButton_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+
+        //borderless window draggable
+        bool mouseDown;
+        private Point offset;
+        private void mouseDown_Event(object sender, MouseEventArgs e)
+        {
+            offset.X = e.X;
+            offset.Y = e.Y;
+            mouseDown = true;
+        }
+
+        private void mouseMove_Event(object sender, MouseEventArgs e)
+        {
+            if (mouseDown == true)
+            {
+                Point currentScreenPos = PointToScreen(e.Location);
+                Location = new Point(currentScreenPos.X - offset.X, currentScreenPos.Y - offset.Y);
+            }
+        }
+
+        private void mouseUp_Event(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+     
     }
 }
